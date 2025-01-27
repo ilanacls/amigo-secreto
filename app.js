5ff976b33f682c97c4f67f64d6922c6c1781c595
ilanacls <ilana_lanahcarneiro@hotmail.com>
2025-01-27 20:01:08 -0300

    cria funções adicionarAmigo e limparCampo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let amigos = document.getElementById('amigo').value;
+    
+    if (amigos == '') {
+        alert ('Por favor, insira um nome.')
+    } else {
+        listaDeAmigos.push(amigos);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    amigos = document.getElementById('amigo');
+    amigos.value = '';
+}
